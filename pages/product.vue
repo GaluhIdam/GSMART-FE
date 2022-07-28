48c173de9b4e50f59ec4050787e4d4c2c3999bac
Jacob Ginting <jacob.ginting@opencloud.id>
2022-07-29 01:30:20 +0700

    CRUD FE Product

--- pages/product.vue
+++ pages/product.vue
@@ -37,25 +37,516 @@
       </div>
       <!--end::Toolbar container-->
     </div>
-    <div class="container mt-20 mb-20">
-      <div class="card shadow-sm">
+    <div class="container mb-10">
+      <div class="card shadow-sm mt-5">
         <div class="card-header">
-          <h3 class="card-title">Title</h3>
+          <h3 class="card-title fw-bold">List of Product</h3>
           <div class="card-toolbar">
-            <button type="button" class="btn btn-sm btn-light">Action</button>
+            <button
+              type="button"
+              class="btn btn-sm btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#modal_create"
+            >
+              Add Product
+            </button>
+            <div
+              class="modal fade"
+              tabindex="-1"
+              id="modal_create"
+              data-bs-backdrop="static"
+            >
+              <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h3 class="modal-title">Add Product</h3>
+
+                    <!--begin::Close-->
+                    <div
+                      class="btn btn-icon btn-sm btn-active-light-primary ms-2"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      <span class="svg-icon svg-icon-1"></span>
+                    </div>
+                    <!--end::Close-->
+                  </div>
+                  <div class="modal-body">
+                    <form v-on:submit.prevent="add">
+                      <div class="form-group mb-3">
+                        <label for="name" class="form-label fw-bold"
+                          >Name</label
+                        >
+                        <input
+                          type="text"
+                          class="form-control"
+                          v-model="name"
+                          :class="{
+                            'is-invalid': errors.name,
+                          }"
+                        />
+                        <span
+                          v-if="errors.name"
+                          class="error invalid-feedback"
+                          >{{ errors.name[0] }}</span
+                        >
+                      </div>
+                      <div class="form-group mb-3">
+                        <label for="description" class="form-label fw-bold"
+                          >Description</label
+                        >
+                        <input
+                          type="text"
+                          class="form-control"
+                          v-model="description"
+                          :class="{
+                            'is-invalid': errors.description,
+                          }"
+                        />
+                        <span
+                          v-if="errors.description"
+                          class="error invalid-feedback"
+                          >{{ errors.description[0] }}</span
+                        >
+                      </div>
+                      <div class="row mt-10">
+                        <div class="col">
+                          <button
+                            type="button"
+                            class="btn btn-light"
+                            data-bs-dismiss="modal"
+                          >
+                            Back
+                          </button>
+                        </div>
+                        <div class="col d-flex justify-content-end">
+                          <button type="submit" class="btn btn-primary">
+                            Save
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="card-body">Lorem Ipsum is simply dummy text...</div>
-        <div class="card-footer">Footer</div>
+        <div class="card-body">
+          <div class="row d-flex align-items-center">
+            <!--begin::Input group-->
+            <div class="position-relative me-md-2">
+              <!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
+              <span
+                class="
+                  svg-icon svg-icon-3 svg-icon-gray-500
+                  position-absolute
+                  top-50
+                  translate-middle
+                  ms-6
+                "
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    opacity="0.5"
+                    x="17.0365"
+                    y="15.1223"
+                    width="8.15546"
+                    height="2"
+                    rx="1"
+                    transform="rotate(45 17.0365 15.1223)"
+                    fill="currentColor"
+                  ></rect>
+                  <path
+                    d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </span>
+              <!--end::Svg Icon-->
+              <input
+                type="text"
+                class="form-control form-control-solid ps-10"
+                name="search"
+                v-model="search"
+                placeholder="Search"
+              />
+            </div>
+            <!--end::Input group-->
+          </div>
+          <div class="py-5">
+            <div class="table-responsive">
+              <table class="table table-row-bordered table-row-gray-200 gy-4">
+                <thead>
+                  <tr class="fw-bold fs-6 text-gray-800">
+                    <th class="text-center">No</th>
+                    <th class="text-center">Name</th>
+                    <th class="text-center">Description</th>
+                    <th class="text-center">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    v-for="(produc, produc_index) in product.data"
+                    :key="produc_index"
+                  >
+                    <td class="text-center">{{ produc_index + 1 }}</td>
+                    <td class="text-center">{{ produc.name }}</td>
+                    <td class="text-center">{{ produc.description }}</td>
+                    <td class="d-flex justify-content-center">
+                      <button class="btn btn-sm btn-light">
+                        <i class="bi bi-toggles text-primary"></i>
+                      </button>
+                      <button
+                        class="btn btn-sm btn-light"
+                        data-bs-toggle="modal"
+                        data-bs-target="#modal_edit"
+                        @click="edit(produc)"
+                      >
+                        <i class="bi bi-pencil-square text-primary"></i>
+                      </button>
+                      <button
+                        class="btn btn-sm btn-light"
+                        v-on:click="remove(produc.id)"
+                      >
+                        <i class="bi bi-trash-fill text-primary"></i>
+                      </button>
+                    </td>
+                  </tr>
+                  <tr v-if="product.data.length < 1">
+                    <td colspan="8">
+                      <div class="text-muted text-center">Data not found</div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <div
+                class="modal fade"
+                tabindex="-1"
+                id="modal_edit"
+                @close="open = false"
+              >
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h3 class="modal-title">Edit Product</h3>
+
+                      <!--begin::Close-->
+                      <div
+                        class="
+                          btn btn-icon btn-sm btn-active-light-primary
+                          ms-2
+                        "
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      >
+                        <span class="svg-icon svg-icon-1"></span>
+                      </div>
+                      <!--end::Close-->
+                    </div>
+
+                    <div class="modal-body">
+                      <form v-on:submit.prevent="update()">
+                        <div class="form-group mb-3">
+                          <label for="name" class="form-label fw-bold"
+                            >Name</label
+                          >
+                          <input
+                            type="text"
+                            class="form-control"
+                            v-model="produc.name"
+                            name="name"
+                          />
+                        </div>
+                        <div class="form-group mb-3">
+                          <label for="description" class="form-label fw-bold"
+                            >Description</label
+                          >
+                          <input
+                            type="text"
+                            class="form-control"
+                            v-model="produc.description"
+                            name="description"
+                          />
+                        </div>
+                        <div class="row mt-10">
+                          <div class="col">
+                            <button
+                              type="button"
+                              class="btn btn-light"
+                              data-bs-dismiss="modal"
+                            >
+                              Back
+                            </button>
+                          </div>
+                          <div class="col d-flex justify-content-end">
+                            <button type="submit" class="btn btn-primary">
+                              Save Changes
+                            </button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer">
+          <div class="row">
+            <div class="col d-flex justify-content-start align-items-center">
+              <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                  <li class="page-item align-self-center">Rows per page:</li>
+                  <li class="page-item">
+                    <select
+                      class="form-control form-control-sm"
+                      v-model="paginate"
+                      v-on:change="list()"
+                    >
+                      <option value="10">10</option>
+                      <option value="25">25</option>
+                      <option value="50">50</option>
+                      <option value="100">100</option>
+                    </select>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div class="col d-flex justify-content-end align-items-center">
+              <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                  <li class="page-item">
+                    <button
+                      type="button"
+                      class="page-link"
+                      :class="{
+                        disabled: !product.prev_page_url,
+                      }"
+                      @click="
+                        product.prev_page_url && list(product.prev_page_url)
+                      "
+                    >
+                      Previous
+                    </button>
+                  </li>
+                  <li
+                    class="page-item"
+                    style="margin-left: 15px; margin-right: 15px"
+                  >
+                    <input
+                      type="text"
+                      class="form-control form-control-sm text-center"
+                      v-model="current_page"
+                      @keypress="directPage"
+                      style="width: 60px"
+                    />
+                  </li>
+                  <li class="page-item">
+                    <button
+                      type="button"
+                      class="page-link"
+                      :class="{
+                        disabled: !product.next_page_url,
+                      }"
+                      @click="
+                        product.next_page_url && list(product.next_page_url)
+                      "
+                    >
+                      Next
+                    </button>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import debounce from 'lodash/debounce'
 export default {
   layout: 'template',
-  name: 'ProductPage',
+  data() {
+    return {
+      open: false,
+      product: {
+        data: [],
+        link: [],
+      },
+      produc: {
+        id: null,
+        name: null,
+        description: null,
+      },
+      name: null,
+      description: null,
+      search: null,
+      order: 'id',
+      by: 'desc',
+      paginate: '10',
+      current_page: null,
+      errors: {
+        name: null,
+        description: null,
+      },
+    }
+  },
+  created() {
+    this.list()
+  },
+  watch: {
+    search: debounce(function () {
+      this.list()
+    }, 500),
+  },
+  methods: {
+    list(paginate) {
+      this.loading()
+      paginate = paginate || `/api/product`
+      this.$axios
+        .get(paginate, {
+          params: {
+            search: this.search,
+            order: this.order,
+            by: this.by,
+            paginate: this.paginate,
+          },
+        })
+        .then((response) => {
+          this.product = response.data.data
+          this.current_page = this.product.current_page
+          Swal.close()
+        })
+        .catch((error) => console.log(error))
+    },
+    directPage: debounce(function () {
+      if (this.current_page < 1) {
+        this.current_page = 1
+      } else if (this.current_page > this.product.last_page) {
+        this.current_page = this.product.last_page
+      }
+      let url = new URL(this.product.first_page_url)
+      let search_params = url.searchParams
+      search_params.set('page', this.current_page)
+      url.search = search_params.toString()
+      let new_url = url.toString()
+      this.list(new_url)
+    }, 500),
+    add() {
+      this.loading()
+      this.$axios
+        .post('/api/product-create', {
+          name: this.name,
+          description: this.description,
+        })
+        .then((result) => {
+          Swal.fire({
+            title: 'Data save successfully!',
+            icon: 'success',
+            confirmButtonText: 'OK',
+          }).then((result) => {
+            this.clearForm()
+            this.list()
+            this.closeModal()
+          })
+        })
+        .catch((error) => {
+          if (error.response.status == 422) {
+            this.errors = error.response.data
+            Swal.fire('Data save failed!', '', 'error')
+          }
+        })
+    },
+    edit(produc) {
+      this.produc.id = produc.id
+      this.produc.name = produc.name
+      this.produc.description = produc.description
+    },
+    update() {
+      this.loading()
+
+      this.$axios
+        .put('/api/product-update/' + this.produc.id, {
+          name: this.produc.name,
+          description: this.produc.description,
+        })
+        .then((result) => {
+          Swal.fire({
+            title: 'Data update successfully!',
+            icon: 'success',
+            confirmButtonText: 'OK',
+          }).then((result) => {
+            this.open = false
+            this.list()
+            this.closeModal()
+          })
+        })
+        .catch((error) => {
+          if (error.response.status == 422) {
+            this.errors = error.response.data
+            Swal.fire('Data update failed!', '', 'error')
+          }
+        })
+    },
+    remove(id) {
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!',
+      })
+        .then((result) => {
+          if (result.isConfirmed) {
+            this.$axios.delete('/api/product-delete/' + id).then((response) => {
+              Swal.fire({
+                title: 'Deleted!',
+                icon: 'success',
+                text: 'Your data has been deleted.',
+                confirmButtonText: 'Ok',
+              }).then((result) => {
+                this.list()
+              })
+            })
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    loading() {
+      Swal.fire({
+        timer: 500,
+        didOpen: () => {
+          Swal.showLoading()
+        },
+        background: 'transparent',
+        allowOutsideClick: false,
+      })
+    },
+    clearForm() {
+      this.name = ''
+      this.description = ''
+      this.errors.name = null
+      this.errors.description = null
+    },
+    closeModal() {
+      document.getElementById('modal_edit').click()
+    },
+  },
 }
 </script>
  <style>
@@ -65,4 +556,8 @@ export default {
 .mb-20 {
   margin-bottom: 20px;
 }
+.mb-10 {
+  margin-bottom: 10px;
+}
 </style>
+

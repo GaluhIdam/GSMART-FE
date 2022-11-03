3d7cd330367ac47baefba559dd5c1c6732a364cf
Surya <surya.permana@opencloud.id>
2022-11-03 10:55:51 +0700

    [fix] memperbaiki ketinggalan pagination

--- pages/product.vue
+++ pages/product.vue
@@ -186,7 +186,7 @@
                       v-model="paginate"
                       @change="list()"
                     >
-                      <option value="1">10</option>
+                      <option value="10">10</option>
                       <option value="25">25</option>
                       <option value="50">50</option>
                       <option value="100">100</option>

7c39415a4bdddfa757c2b33a37c6668d8e22eb2e
Surya <surya.permana@opencloud.id>
2022-11-11 05:09:24 +0700

    [fix] merubah readonly menjadi disabled

--- pages/role.vue
+++ pages/role.vue
@@ -273,7 +273,8 @@
                   type="text"
                   class="form-control"
                   v-model="role.name"
-                  readonly
+                  disabled
+                  id="readOnly"
                   :class="{
                     'is-invalid': errors.name,
                   }"
@@ -605,3 +606,9 @@ export default {
   },
 }
 </script>
+
+<style>
+#readOnly {
+  background-color:#f0f0f5;
+}
+</style>

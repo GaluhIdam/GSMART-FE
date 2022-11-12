76166d07f805dad46c954b086ac2eb963e5c02b2
Farhan Hadi Lutfi <farhan.hadi@opencloud.id>
2022-11-12 19:16:05 +0700

    [update] update total sales plan

--- pages/view-prospect-pbth.vue
+++ pages/view-prospect-pbth.vue
@@ -38,8 +38,14 @@
         <div class="row">
           <div class="col-md-5 p-16">
             <span class="btn btn-light btn-sm p-5">
-              <img :src=customer_image v-if="customer_image" class="img-fluid" width="25" height="25">
-              <p v-else></p>
+              <div v-if="customer_image == null">
+                <span class="btn btn-light btn-sm p-20 fs-1 fw-bold">
+                  {{ initial }}
+                </span>
+              </div>
+              <div v-else>
+              <img :src=customer_image class="img-fluid" width="25" height="25">
+              </div>
             </span>
             <span class="badge text-muted text-bg-light d-block text-start mt-2">PBTH</span>
             <h2 class="mt-1">Airframe</h2>
@@ -253,9 +259,14 @@
                     <td class="text-center">{{ data.flight_hour }}</td>
                     <td class="text-center">{{ data.rate }} %</td>
                     <td class="text-center">{{ data.planFH }}</td>
-                    <td class="text-center" style="color: #50CD89" v-if="data.value">{{ formatNumber(data.value) }}</td>
+                    <td class="text-center" style="color: #50CD89" v-if="data.value">${{ formatNumber(data.value) }}</td>
                     <td class="text-center" style="color: #50CD89" v-else>0</td>
                   </tr>
+                  <tr><td class="text-center"></td>
+                  <td class="text-center"></td>
+                  <td class="text-center"></td>
+                  <td class="text-center fs-3 fw-bold">Total Sales Plan</td>
+                  <td class="text-center fs-3 fw-bold" style="color: #50CD89">${{ formatNumber(total_sales_plan) }}</td></tr>
                 </tbody>
               </table>
             </div>
@@ -276,7 +287,7 @@ export default {
       customer_options: [],
       pbth: [],
       sales_plan: [],
-      registration: [],
+      registration: null,
       customer_image: [],
       market_share: [],
       deviation: [],
@@ -299,6 +310,9 @@ export default {
         customer_id: null,
         title: null,
       },
+      total_sales_plan: null,
+      pbth_length: null,
+      initial: null,
       }
     },
   created() {
@@ -352,7 +366,7 @@ export default {
     checkRole(){
       if(this.role == 'AMS' || this.role == 'Administrator'){
       } else {
-        this.$router.push('/');
+        this.$router.push('/my-prospect');
         this.authMessage()
       }
     },
@@ -394,12 +408,14 @@ export default {
           this.pbth = response.data.data.prospect
           // Data Customer
           this.customer_image = response.data.data.customer.full_path
+          this.customer_name = response.data.data.customer.name
           // Get Customer ID
           this.customer_id = response.data.data.customer.id
           // Registration Format
           this.registration = response.data.data.registration
           // Sales Plan Value
           this.sales_plan = response.data.data.sales_plan
+          this.initial = this.customer_name.substring(0,1)
           if(this.sales_plan.length > 0) {
             this.PBTHMessage()
             this.$router.push('/my-prospect');
@@ -420,11 +436,17 @@ export default {
       })
     },
     calculateSalesPlan(){
+      this.pbth_length = this.pbth.length
+      this.pbth_length_min = this.pbth.length - 1
       this.pbth.forEach(element => {
         element.month = element.month + ' '
         element.planFH = element.flight_hour * this.convidenve_level
         element.value = element.planFH * element.rate
       });
+      this.total_sales_plan = 0
+      for(let i = 0; i < this.pbth_length; i++){
+        this.total_sales_plan += this.pbth[i].value
+      }
     },
     createSalesPlan(){
       this.loading()
@@ -522,6 +544,3 @@ export default {
     }
   }
 </script>
-
-<style>
-</style>

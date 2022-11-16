a57fa8e0a15cd7bfef81250920e7500d5e9e43cf
Galuh Idam <galuh.danutirto@opencloud.id>
2022-11-17 04:33:14 +0700

    [FIXED] Fix Bug Cookies Token

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,8 +61,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://dev-gsmartbe.gmf-aeroasia.co.id',
-    // baseURL: 'http://127.0.0.1:8000',
+    // baseURL: 'https://dev-gsmartbe.gmf-aeroasia.co.id',
+    baseURL: 'http://127.0.0.1:8000',
     credentials: true,
   },
 
@@ -98,6 +98,7 @@ export default {
           global: true,
           required: true,
           type: 'Bearer',
+          maxAge: 2592000,
         },
         user: {
           property: 'user',

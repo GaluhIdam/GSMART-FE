6c7915e4818e746a92fa30eb2f2d88a769edc248
Galuh Idam <galuh.danutirto@opencloud.id>
2022-11-25 03:40:41 +0700

    [UPDATED] Updated Cookie Search

--- pages/my-salesplan-table.vue
+++ pages/my-salesplan-table.vue
@@ -487,47 +487,52 @@
                 <div class="card-body">
                     <!-- Search -->
                     <div class="row d-flex align-items-center mb-5">
-                    <div class="position-relative me-md-2">
-                        <span
-                        class="
-                            svg-icon svg-icon-3 svg-icon-gray-500
-                            position-absolute
-                            top-50
-                            translate-middle
-                            ms-6
-                        "
-                        >
-                        <svg
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <rect
-                            opacity="0.5"
-                            x="17.0365"
-                            y="15.1223"
-                            width="8.15546"
-                            height="2"
-                            rx="1"
-                            transform="rotate(45 17.0365 15.1223)"
-                            fill="currentColor"
-                            ></rect>
-                            <path
-                            d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
-                            fill="currentColor"
-                            ></path>
-                        </svg>
-                        </span>
-                        <input
-                        type="text"
-                        class="form-control form-control-solid ps-10"
-                        name="search"
-                        v-model="search"
-                        placeholder="Search"
-                        />
-                    </div>
+                        <div class="col-11">
+                          <div class="row">
+                            <span
+                          class="
+                              svg-icon svg-icon-3 svg-icon-gray-500
+                              position-absolute
+                              top-50
+                              translate-middle
+                              ms-6
+                          "
+                          >
+                          <svg
+                              width="24"
+                              height="24"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                          >
+                              <rect
+                              opacity="0.5"
+                              x="17.0365"
+                              y="15.1223"
+                              width="8.15546"
+                              height="2"
+                              rx="1"
+                              transform="rotate(45 17.0365 15.1223)"
+                              fill="currentColor"
+                              ></rect>
+                              <path
+                              d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
+                              fill="currentColor"
+                              ></path>
+                          </svg>
+                          </span>
+                          <input
+                          type="text"
+                          class="form-control form-control-solid ps-10"
+                          name="search"
+                          v-model="search"
+                          placeholder="Search"
+                          />
+                          </div>
+                        </div>
+                        <div class="col">
+                          <button class="btn btn-danger btn-sm" @click="removeSearch()">Clear</button>
+                        </div>
                     </div>
                     <!-- Table -->
                     <div class="table-responsive">
@@ -1159,7 +1164,7 @@ export default {
         progress: null,
         status: null,
       },
-      search: null,
+      search: this.$cookies.get('search'),
 
       maintenance_option: [],
       product_option: [],
@@ -1279,7 +1284,7 @@ export default {
   watch: {
     search: debounce(function () {
       this.listTable()
-    }, 500),
+    }, 800),
   },
   created() {
     this.listTable()
@@ -1329,7 +1334,7 @@ export default {
       this.$axios
         .get(paginate, {
           params: {
-            search: this.search,
+            search: this.search == null ? null : this.search,
             order: this.order,
             by: this.by,
             paginate: this.paginate,
@@ -1358,7 +1363,30 @@ export default {
         })
         .catch((error) => console.log(error))
     },
+    removeSearch() {
+      this.search = null
+      this.$cookies.set('search', this.search, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7
+      })
+    },
     runCookie() {
+      //search
+      if(this.search) {
+          this.$cookies.set('search', this.search, {
+            path: '/',
+            maxAge: 60 * 60 * 24 * 7
+          })
+        } else {
+          this.$cookies.set('search', this.search, {
+            path: '/',
+            maxAge: 60 * 60 * 24 * 7
+          })
+        }
+      if(this.$cookies.get('search')) {
+          this.search = this.$cookies.get('search')
+        }
+
       //start date
       if(this.start_date) {
           this.$cookies.set('start_date', this.start_date, {
